329adffe874ac21ba97dd21f6fe0ccf0c1e28461
cysnake4713 <cysnake4713@gmail.com>
2015-08-09 17:34:40 +0800

    [IMP]xiao_invoice:添加实体发票记录信息

--- addons/xiao_invoice/models/invoice.py
+++ addons/xiao_invoice/models/invoice.py
@@ -11,4 +11,17 @@ class InvoiceInherit(models.Model):
     _inherit = 'account.invoice'
 
     invoice_company_name = fields.Char('Invoice Company Name')
+    real_invoice_ids = fields.One2many('account.invoice.real.line', 'invoice_id', 'Real Invoices')
 
+
+class RealInvoice(models.Model):
+    _name = 'account.invoice.real.line'
+    _rec_name = 'real_invoice_number'
+    _order = 'real_invoice_date desc'
+    _description = 'Account Invoice Real Line'
+
+    invoice_id = fields.Many2one('account.invoice', 'Related Invoice', ondelete='cascade')
+
+    real_invoice_number = fields.Char('Real Invoice Number')
+    real_invoice_date = fields.Date('Real Invoice Date', default=lambda self: fields.Date.today())
+    comment = fields.Char('Comment')
