dc8a58fa12ff6c32529abc41d6400854c3116c4b
cysnake4713 <cysnake4713@gmail.com>
2015-08-26 12:17:31 +0800

    [UPD]xiao_wechat_stock:页面开发完成,测试扫码功能

--- addons/xiao_wechat_stock/controllers/main.py
+++ addons/xiao_wechat_stock/controllers/main.py
@@ -13,8 +13,8 @@ _logger = logging.getLogger(__name__)
 
 
 class StockWebsite(http.Controller):
-    @http.route('/mobile/stock/scan/<code>', type='http', auth='user_wechat')
-    def stock_scan(self, code):
+    @http.route('/mobile/stock/scan/list/<code>', type='http', auth='user_wechat')
+    def stock_list(self, code, **kw):
         client = request.registry['odoosoft.wechat.enterprise.account'].get_client(request.cr, request.uid, code, context=request.context)
         if client:
             noncestr = 'Wm3WZYTPz0wzccnW'
@@ -25,9 +25,37 @@ class StockWebsite(http.Controller):
                 'corp_id': client.corp_id,
                 'noncestr': noncestr,
                 'timestamp': timestamp,
-                'signature': client.jsapi.get_jsapi_signature(noncestr, ticket, timestamp, url)
+                'signature': client.jsapi.get_jsapi_signature(noncestr, ticket, timestamp, url),
+                'debug': 'true' if 'debug' in kw else 'false',
 
             }
-            return request.render('xiao_wechat_stock.scan', qcontext={'config': config})
+            # process list
+            picking_ids = request.registry['stock.picking'].search(request.cr, request.uid,
+                                                                   [('state', 'in', ['confirmed', 'partially_available', 'assigned'])],
+                                                                   context=request.context)
+            result = request.registry['stock.picking'].browse(request.cr, request.uid, picking_ids, context=request.context)
+
+            return request.render('xiao_wechat_stock.stock_list', qcontext={'config': config, 'result': result, 'code': code})
         else:
             return NotFound()
+
+    @http.route('/mobile/stock/scan/detail/<code>/<id>', type='http', auth='user_wechat')
+    def stock_form(self, code, id, **kw):
+        client = request.registry['odoosoft.wechat.enterprise.account'].get_client(request.cr, request.uid, code, context=request.context)
+        if client:
+            noncestr = 'Wm3WZYTPz0wzccnW'
+            ticket = client.jsapi.get_jsapi_ticket()
+            timestamp = client.jsapi.session.get('jsapi_ticket_expires_at', 0)
+            url = request.httprequest.base_url
+            config = {
+                'corp_id': client.corp_id,
+                'noncestr': noncestr,
+                'timestamp': timestamp,
+                'signature': client.jsapi.get_jsapi_signature(noncestr, ticket, timestamp, url),
+                'debug': 'true' if 'debug' in kw else 'false',
+
+            }
+
+            result = request.registry['stock.picking'].browse(request.cr, request.uid, int(id), context=request.context)
+            return request.render('xiao_wechat_stock.stock_form', qcontext={'config': config, 'result': result, 'code': code})
+        return NotFound()
--- addons/xiao_wechat_stock/static/src/js/stock_scan.js
+++ addons/xiao_wechat_stock/static/src/js/stock_scan.js
@@ -1,10 +1,25 @@
-$('#test').on('click', function (e) {
-    wx.scanQRCode({
-        desc: 'scanQRCode desc',
-        needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
-        scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
-        success: function (res) {
-            $('#result').html = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
-        }
+$(function () {
+    $('.return-list').on('click', function (e) {
+        e.preventDefault();
+        window.location.href = this.children[0].href;
     });
+
+    $('.scan-wechat').on('click', function (e) {
+        wx.scanQRCode({
+            desc: 'scanQRCode desc',
+            needResult: 1, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+            scanType: ["qrCode", "barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+            success: function (res) {
+                $('.carrier_tracking_ref').val(res.resultStr);
+            }
+        });
+    });
+
+    $('.carrier_tracking_ref_clear').on('click', function (e) {
+        e.preventDefault();
+         $('.carrier_tracking_ref').val('');
+    });
+
+
 });
+
