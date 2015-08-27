1a334cb85b3337e3e2157de79409fb500bdcff94
cysnake4713 <cysnake4713@gmail.com>
2015-08-27 23:35:02 +0800

    [UPD]xiao_wechat_stock:wechatjs 同主页面分离

--- addons/xiao_wechat_stock/controllers/main.py
+++ addons/xiao_wechat_stock/controllers/main.py
@@ -15,30 +15,14 @@ _logger = logging.getLogger(__name__)
 class StockWebsite(http.Controller):
     @http.route('/mobile/stock/scan/list/<code>', type='http', auth='user_wechat')
     def stock_list(self, code, **kw):
-        client = request.registry['odoosoft.wechat.enterprise.account'].get_client(request.cr, request.uid, code, context=request.context)
-        if client:
-            noncestr = 'Wm3WZYTPz0wzccnW'
-            ticket = client.jsapi.get_jsapi_ticket()
-            timestamp = client.jsapi.session.get('jsapi_ticket_expires_at', 0)
-            url = request.httprequest.base_url
-            config = {
-                'corp_id': client.corp_id,
-                'noncestr': noncestr,
-                'timestamp': timestamp,
-                'signature': client.jsapi.get_jsapi_signature(noncestr, ticket, timestamp, url),
-                'debug': 'true' if 'debug' in kw else 'false',
+        # process list
+        picking_ids = request.registry['stock.picking'].search(request.cr, request.uid,
+                                                               [('state', 'in', ['confirmed', 'partially_available', 'assigned'])],
+                                                               order='id desc',
+                                                               context=request.context)
+        result = request.registry['stock.picking'].browse(request.cr, request.uid, picking_ids, context=request.context)
 
-            }
-            # process list
-            picking_ids = request.registry['stock.picking'].search(request.cr, request.uid,
-                                                                   [('state', 'in', ['confirmed', 'partially_available', 'assigned'])],
-                                                                   order='id desc',
-                                                                   context=request.context)
-            result = request.registry['stock.picking'].browse(request.cr, request.uid, picking_ids, context=request.context)
-
-            return request.render('xiao_wechat_stock.stock_list', qcontext={'config': config, 'result': result, 'code': code})
-        else:
-            return NotFound()
+        return request.render('xiao_wechat_stock.stock_list', qcontext={'result': result, 'code': code})
 
     @http.route('/mobile/stock/scan/detail/<code>/<id>', type='http', auth='user_wechat')
     def stock_form(self, code, id, **kw):
