96fb7820e0080a34a6fcd2e99ea7746daa4dd762
cysnake4713 <cysnake4713@gmail.com>
2015-11-20 12:55:13 +0800

    [UPD]xiao_interface:添加用户同步

--- addons/xiao_interface/models/partner.py
+++ addons/xiao_interface/models/partner.py
@@ -20,7 +20,7 @@ class ResPartner(models.Model):
     def sync_tianv_data(self):
         companys = self.search_read([('is_company', '=', True), ('tianv_id', '!=', 0)], ['tianv_id', 'tianv_last_update'])
         company_maps = {c['tianv_id']: (c['id'], c['tianv_last_update']) for c in companys}
-        users = self.search_read([('is_company', '=', False)], ['tianv_id', 'tianv_last_update'])
+        users = self.search_read([('is_company', '=', False), ('tianv_id', '!=', 0)], ['tianv_id', 'tianv_last_update'])
         user_maps = {c['tianv_id']: (c['id'], c['tianv_last_update']) for c in users}
 
         remote_result = client.GetPartnerIds()
@@ -48,6 +48,35 @@ class ResPartner(models.Model):
                     'industry': company_info['industry'],
                     'company_type': company_info['companyType'],
                     'tianv_last_update': remote_company['lastUpdate'],
+                })
+
+        # sync user
+        for remote_user in remote_result['users']:
+            # new
+            if remote_user['id'] not in user_maps:
+                user_info = client.GetUserInfo(id=remote_user['id'])
+                new_user = self.create({
+                    'name': user_info['name'],
+                    'tianv_id': user_info['id'],
+                    'tianv_last_update': remote_user['lastUpdate'],
+                    'zip': user_info['zip'],
+                    'street': user_info['address'],
+                    'email': user_info['mail'],
+                    'parent_id': company_maps[user_info['companyId']][0] if user_info['companyId'] in company_maps else 0,
+                    'is_company': False,
+                    'company_id': self.env['ir.config_parameter'].get_param('interface.partner.company.default')
+                })
+            # update
+            elif remote_user['lastUpdate'] != user_maps[remote_user['id']][1]:
+                user_info = client.GetUserInfo(id=remote_user['id'])
+                update_user = self.browse(user_maps[remote_user['id']][0]).write({
+                    'name': user_info['name'],
+                    'tianv_id': user_info['id'],
+                    'tianv_last_update': remote_user['lastUpdate'],
+                    'zip': user_info['zip'],
+                    'street': user_info['address'],
+                    'email': user_info['mail'],
+                    'parent_id': company_maps[user_info['companyId']][0] if user_info['companyId'] in company_maps else 0,
                 })
 
     @api.multi
