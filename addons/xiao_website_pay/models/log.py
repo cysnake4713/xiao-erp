97777aaef36bcc939e4b0125eb02323a1a7d09cc
cysnake4713 <cysnake4713@gmail.com>
2016-01-15 21:39:15 +0800

    [UPD]xiao_website_pay:添加记录明细

--- addons/xiao_website_pay/models/log.py
+++ addons/xiao_website_pay/models/log.py
@@ -8,10 +8,13 @@ from openerp.tools.translate import _
 
 class WebsitePayLog(models.Model):
     _name = 'xiao.website.pay.log'
-    _rec_name = 'name'
+    _rec_name = 'tianv_id'
     _description = 'Xiao Website Pay Log'
+    _order = 'id desc'
 
-    name = fields.Char('Pay Description')
-    order_id = fields.Many2one('sale.order', 'Order')
+    tianv_id = fields.Char('Tianv ID', required=True)
+    related_order_ids = fields.Many2many('sale.order', 'tianv_pay_order_rel', 'log_id', 'order_id', 'Related Orders')
+    closed_order_ids = fields.Many2many('sale.order', 'tianv_pay_close_order_rel', 'log_id', 'order_id', 'Closed Orders')
     price = fields.Float('Price', digits=(10, 2))
     pay_datetime = fields.Datetime('Pay Datetime')
+    is_reconciled = fields.Boolean('Is Reconciled')
