fe84e93b886adf96535f218ba4599408e91c6757
cysnake4713 <cysnake4713@gmail.com>
2016-02-01 00:43:19 +0800

    [UPD]xiao_website_pay:添加编号，状态

--- addons/xiao_website_pay/models/log.py
+++ addons/xiao_website_pay/models/log.py
@@ -8,10 +8,12 @@ from openerp.tools.translate import _
 
 class WebsitePayLog(models.Model):
     _name = 'xiao.website.pay.log'
-    _rec_name = 'tianv_id'
+    _rec_name = 'name'
     _description = 'Xiao Website Pay Log'
     _order = 'id desc'
 
+    name = fields.Char('Payment No.')
+    state = fields.Char('State')
     tianv_id = fields.Char('Tianv ID', required=True)
     related_order_ids = fields.Many2many('sale.order', 'tianv_pay_order_rel', 'log_id', 'order_id', 'Related Orders')
     closed_order_ids = fields.Many2many('sale.order', 'tianv_pay_close_order_rel', 'log_id', 'order_id', 'Closed Orders')
